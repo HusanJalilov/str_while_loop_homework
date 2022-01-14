090bf3711b12a38498b3d3dde24b71c028c7771b
Hu$an <76938934+HusanJalilov@users.noreply.github.com>
2022-01-15 00:48:53 +0500

    Task 01

--- While01.py
+++ While01.py
@@ -1,3 +1,4 @@
+
 def main(s):
     """
     A variable of type str is given. Find how many numbers it contains and return.
@@ -6,4 +7,14 @@ def main(s):
     Returns:
         int: return answer
     """
-    return
+    m=0
+    idx=0
+    while idx<len(s):
+        if s[idx].isdigit()==True:
+            m+=1
+    
+        idx=idx+1
+    print(m)
+
+main("22as23a2")
+    
